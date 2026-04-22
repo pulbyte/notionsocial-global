abc1564f6e4e4a68d28b84b8ed4749589d4a0dfc
pulbyte <pulbyte@gmail.com>
2026-04-22 16:56:00 +0530

    chore(jest): modernize ts-jest config to transform API (drops deprecation warning)

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,8 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/tests/**/*.test.ts'],
-    globals: {
-        'ts-jest': {
-            isolatedModules: true,
-        },
+    // TODO: remove isolatedModules once Task 6 resolves v4 errors in src/notion.ts
+    transform: {
+        '^.+\\.tsx?$': ['ts-jest', {isolatedModules: true}],
     },
 };
